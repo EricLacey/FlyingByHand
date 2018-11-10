9f748771e71edfd7e4e760fa65120914cdacd239
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2018-11-10 04:12:14 -0500

    Updated with code from notes as base

---
+++ src/ProjectConstants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ofMain.h"
+
+
+namespace ProjectConstants {
+	//app consts
+	static const int PROJ_WINDOW_RES_X = 1280;
+	static const int PROJ_WINDOW_RES_Y = 720;
+	static const float PROJ_DESIRED_FRAMERATE = 60.0f;
+};
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,11 +1,24 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxLeapC.h"
+
+#include "ProjectConstants.h"
+
 
 class ofApp : public ofBaseApp {
 
 public:
     void setup();
+	void onLeapFrame(Leap::Frame frame);
     void update();
     void draw();
+
+	ofxLeapC::Device		m_device;
+	Leap::Frame				m_frame;
+
+	ofVec3f					m_palmPos;
+	ofVec3f					m_palmRot;
+	float					m_pinchStrength;
+	float					m_grabStrength;
 };
