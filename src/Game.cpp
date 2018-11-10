2b260e57fc4b47115beddfaa825c276b7a069beb
Delta <98hawkeye@gmail.com>
2018-11-10 18:33:49 -0500

    updated game class

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,17 +6,17 @@ void Game::InitGame()
 	LoadBGImages();
 	LoadObjectImages();
 
-	gameSpeed = 1000.0f;
-//	m_b1.setOrder(1);
-	//m_b2.setOrder(2);
-	//m_b3.setOrder(3);
+	gameSpeed = 500.0f;
+	m_b1.setOrder(1);
+	m_b2.setOrder(2);
+	m_b3.setOrder(3);
 	m_b1.setX(0.0);
 	m_b2.setX(0.0);
 	m_b3.setX(0.0);
 
 	m_b1.setY(0.0);
-	m_b2.setY(0.0 - m_b2.getImage().getHeight());
-	m_b3.setY(0.0 - m_b2.getImage().getHeight() - m_b3.getImage().getHeight());
+	m_b2.setY(0.0 - m_b2.getImage().getHeight() + 240);
+	m_b3.setY(0.0 - m_b2.getImage().getHeight() - m_b3.getImage().getHeight() + 480);
 
 	//coins
 	for (int i = 0; i < 10; ++i)
@@ -51,50 +51,93 @@ void Game::Update(float deltaTime)
 
 	if (m_b1.getY() >= 1080)
 	{
-		m_b1.setY(m_b3.getY() - m_b3.getImage().getHeight());
+		m_b1.setY(m_b3.getY() - m_b3.getImage().getHeight() + 240);
 	//	cout << "moving 1 to top" << endl;
 		m_b1.setImage(BGImages[rand() % 7]);
+		m_b1.setOrder(3);
+		m_b2.setOrder(1);
+		m_b3.setOrder(2);
 
 	}
 	else if (m_b2.getY() >= 1080)
 	{
-		m_b2.setY(m_b1.getY() - m_b1.getImage().getHeight());
+		m_b2.setY(m_b1.getY() - m_b1.getImage().getHeight() + 240);
 	//	cout << "moving 2 to top" << endl;
 		m_b2.setImage(BGImages[rand() % 7]);
+		m_b1.setOrder(2);
+		m_b2.setOrder(3);
+		m_b3.setOrder(1);
 
 	}
 	else if (m_b3.getY() >= 1080)
 	{
-		m_b3.setY(m_b2.getY() - m_b2.getImage().getHeight());
+		m_b3.setY(m_b2.getY() - m_b2.getImage().getHeight() + 240);
 	//	cout << "moving 3 to top" << endl;
 		m_b3.setImage(BGImages[rand() % 7]);
-
+		m_b1.setOrder(1);
+		m_b2.setOrder(2);
+		m_b3.setOrder(3);
 	}
 
 }
 
 void Game::Draw()
-{
-	//ofPushMatrix();
-	ofPushMatrix();
-	ofTranslate(m_b1.getX(), m_b1.getY());
-	m_b1.getImage().draw(0, 0);
-	ofPopMatrix();
-
-	//ofPushMatrix();
-//	ofTranslate(0, m_b2.getY());
-	ofPushMatrix();
-	ofTranslate(m_b2.getX(), m_b2.getY());
-	m_b2.getImage().draw(0, 0);
-	ofPopMatrix();
-
-	//ofPushMatrix();
-	//ofTranslate(0, m_b3.getY());
-	ofPushMatrix();
-	ofTranslate(m_b3.getX(), m_b3.getY());
-	m_b3.getImage().draw(0, 0);
-	ofPopMatrix();
+{	
 	
+	if (m_b1.getOrder() == 3)
+	{
+		ofPushMatrix();
+		ofTranslate(m_b1.getX(), m_b1.getY());
+		m_b1.getImage().draw(0, 0);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofTranslate(m_b3.getX(), m_b3.getY());
+		m_b3.getImage().draw(0, 0);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofTranslate(m_b2.getX(), m_b2.getY());
+		m_b2.getImage().draw(0, 0);
+		ofPopMatrix();
+
+	}
+	else if (m_b1.getOrder() == 2)
+	{
+		
+		ofPushMatrix();
+		ofTranslate(m_b2.getX(), m_b2.getY());
+		m_b2.getImage().draw(0, 0);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofTranslate(m_b1.getX(), m_b1.getY());
+		m_b1.getImage().draw(0, 0);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofTranslate(m_b3.getX(), m_b3.getY());
+		m_b3.getImage().draw(0, 0);
+		ofPopMatrix();
+	}
+	else
+	{
+		ofPushMatrix();
+		ofTranslate(m_b3.getX(), m_b3.getY());
+		m_b3.getImage().draw(0, 0);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofTranslate(m_b2.getX(), m_b2.getY());
+		m_b2.getImage().draw(0, 0);
+		ofPopMatrix();
+
+		ofPushMatrix();
+		ofTranslate(m_b1.getX(), m_b1.getY());
+		m_b1.getImage().draw(0, 0);
+		ofPopMatrix();
+	
+	}
 
 
 	//for (int i = 0; i < items.size(); ++i)
@@ -113,14 +156,14 @@ void Game::LoadBGImages()
 	ofImage Canal3B;
 	ofImage Canal3C;
 	ofImage Canal5A;
-	Canal1A.load("C1A.jpg");
-	Canal1B.load("C1B.jpg");
-	Canal2A.load("C2A.jpg");
-	Canal2B.load("C2B.jpg");
-	Canal3A.load("C3A.jpg");
-	Canal3B.load("C3B.jpg");
-	Canal3C.load("C3C.jpg");
-	Canal5A.load("C5A.jpg");
+	Canal1A.load("C1A.png");
+	Canal1B.load("C1B.png");
+	Canal2A.load("C2A.png");
+	Canal2B.load("C2B.png");
+	Canal3A.load("C3A.png");
+	Canal3B.load("C3B.png");
+	Canal3C.load("C3C.png");
+	Canal5A.load("C5A.png");
 	BGImages.push_back(Canal1A);
 	BGImages.push_back(Canal1B);
 	BGImages.push_back(Canal2A);
