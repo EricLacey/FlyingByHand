c3d3971006a98d140c0dee85e4022af484d3d233
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2018-11-10 18:31:53 -0500

    cleanup from merge branches

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,7 +9,7 @@ void ofApp::setup()
 	m_grabAnim = false;
 	m_pinchAnim = false;
 
-<<<<<<< HEAD
+
 	//setup of leap motion
 	m_device.connectEventHandler(&ofApp::onLeapFrame, this);
 
@@ -17,13 +17,13 @@ void ofApp::setup()
 	ofSetFrameRate(ProjectConstants::PROJ_DESIRED_FRAMERATE);
 
 
-=======
+
 	//ofSetFrameRate(ProjectConstants::PROJ_DESIRED_FRAMERATE);
 
 	newGame.InitGame();
 	gameTime.Update();
 
->>>>>>> Game/AI-branch
+
 }
 
 void ofApp::onLeapFrame(Leap::Frame frame)
@@ -34,12 +34,12 @@ void ofApp::onLeapFrame(Leap::Frame frame)
 
 //--------------------------------------------------------------
 void ofApp::update(){
-<<<<<<< HEAD
-	//update leap motion
-=======
 
+	//update game
 	newGame.Update(gameTime.GetDeltaTime());
->>>>>>> Game/AI-branch
+	gameTime.Update();
+
+	//update leap motion
 	m_device.update();
 
 	//creating list of hand objects from frame
@@ -55,23 +55,16 @@ void ofApp::update(){
 		//pulling palmPos from hand and converting to vec3 for use
 		const Leap::Vector palmPos = hand.palmPosition();
 		const ofVec3f ofPalmPos = ofxLeapC::toVec3(hand.palmPosition());
-<<<<<<< HEAD
 
 		//scaling palm position 
 		m_palmPos.x = ofPalmPos.x * 7.0f;
 		m_palmPos.z = ofPalmPos.z * 5.0f;
-
-
 		m_palmPos.x = ofClamp(m_palmPos.x, -(float)ProjectConstants::PROJ_WINDOW_RES_X / 2.0f, (float)ProjectConstants::PROJ_WINDOW_RES_X / 2.0f);
 		m_palmPos.z = ofClamp(m_palmPos.z, -(float)ProjectConstants::PROJ_WINDOW_RES_Y / 2.0f, (float)ProjectConstants::PROJ_WINDOW_RES_Y / 2.0f);
-
 		m_palmPos += ofVec3f((float)ProjectConstants::PROJ_WINDOW_RES_X / 2.0f, 0.0f, (float)ProjectConstants::PROJ_WINDOW_RES_Y / 2.0f);
 
 
 		//pulling grab and pinch gestures from hand
-=======
-		
->>>>>>> Game/AI-branch
 		m_grabStrength = hand.grabStrength();
 		m_pinchStrength = hand.pinchStrength();
 
@@ -82,13 +75,9 @@ void ofApp::update(){
 			ofRadToDeg(hand.direction().yaw()),
 			ofRadToDeg(hand.direction().roll())
 		);
-
-
-
 		break;
 	}
 
-<<<<<<< HEAD
 	//set grab animation if grabbing
 	if (m_grabStrength >= 0.8) {
 		m_grabAnim = true;
@@ -102,9 +91,7 @@ void ofApp::update(){
 	else if (!m_grabAnim && m_charScale < 1.0) {
 		m_charScale += 0.01;
 	}
-=======
-	gameTime.Update();
->>>>>>> Game/AI-branch
+
 }
 
 //--------------------------------------------------------------
